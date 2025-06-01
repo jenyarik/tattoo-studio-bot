cb83c7271287ee75137bac2479f8459f6f0c383b
jenyarik <129371101+jenyarik@users.noreply.github.com>
2025-06-01 15:08:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dbQueries = require('./db.queries');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 app.use(cors());
 
 const app = express();
