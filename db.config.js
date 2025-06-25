b52f7713b3e94ddc84f67e2a9f995700dc76703f
jenyarik <129371101+jenyarik@users.noreply.github.com>
2025-06-25 11:48:51 +0500

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -1,12 +1,8 @@
 const { Pool } = require('pg');
-require('dotenv').config(); // Подключаем .env
+require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        require: true,
-        rejectUnauthorized: true, // Или можно убрать эту строку - по умолчанию true
-    },
 });
 
 module.exports = {
