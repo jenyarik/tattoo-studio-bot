9b193b1fbae0851662e80a206207f2f78c75948b
jenyarik <129371101+jenyarik@users.noreply.github.com>
2025-06-25 12:00:51 +0500

    Update db.queries.js

--- db.queries.js
+++ db.queries.js
@@ -1,9 +1,12 @@
 // db.queries.js
 const { Pool } = require('pg');
-require('dotenv').config();
+require('dotenv').config(); // Подключаем .env
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // REMOVE IN PRODUCTION
+    },
 });
 
 async function query(text, params) {
