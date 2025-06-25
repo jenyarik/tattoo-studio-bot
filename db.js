d721355c04ddcf7773b18afb7d72f26150d83de2
jenyarik <129371101+jenyarik@users.noreply.github.com>
2025-06-25 14:29:42 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
-// db/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // REMOVE IN PRODUCTION
-  }
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
